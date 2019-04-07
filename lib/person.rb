3d5b3c2551f0bd708047d39b85cf1aa32f2cd18a
Luka Baramishvili <lukabaramishvili@gmail.com>
2019-04-07 16:55:56 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,6 +2,12 @@ require 'pry'
 
 class Person
   
-  attr_accessor :name 
+  attr_accessor :bank_account, :happiness, :hygiene
+  attr_reader :name
   
+  def initialize(name, bank_account=25, happiness=8, hygiene=8)
+    @name = name 
+    @bank_account = bank_account
+    @happiness = happiness
+    @hygiene =hygiene
 end
